1676a07808790707ecab84605b6b30148475aca8
orlevy13 <orlevy13d@gmail.com>
2020-04-29 21:07:26 +0300

    css changes

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -3,24 +3,18 @@ import SearchBar from './SearchBar.jsx'
 const { NavLink } = ReactRouterDOM
 
 export default class AppHeader extends React.Component {
-    state = {
-        isShown: false
-    }
-    toggleNav = () => {
-        this.setState(prevState => ({isShown: !prevState.isShown}))
-    }
+
     render() {
         return (
             <header className="flex space-between">
                 <NavLink exact to='/'>
-                <div className="logo">
-                    <img className="logo-img" src="./assets/img/logo.png" />
-                    <span className="logo-txt">Appsus</span>
-                    <p>First React project</p>
-                </div></NavLink>
+                    <div className="logo">
+                        <img className="logo-img" src="./assets/img/logo.png" />
+                        <span className="logo-txt">Appsus</span>
+                        <p>First React project</p>
+                    </div></NavLink>
                 <SearchBar></SearchBar>
-                <button onClick={this.toggleNav}>%%</button>
-                {this.state.isShown && <NavBar />}
+                <NavBar />
             </header>
         )
     }
