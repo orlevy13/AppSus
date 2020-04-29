be88429a089463d189fd92e4f2a7692dd3940eb2
orlevy13 <orlevy13d@gmail.com>
2020-04-29 16:06:46 +0300

    changed image srcs

--- apps/email/cmps/NavBar.jsx
+++ apps/email/cmps/NavBar.jsx
@@ -7,13 +7,13 @@ export default function NavBar() {
             <ul className="nav-bar clean-list">
                 <NavLink activeClassName="active" exact to='/email'>
                     <li className="flex space-between align-center">
-                        <span className="flex"><img className="sidebar-icon" height="18" src="/apps/email/assets/imgs/inbox.png" />Inbox</span><EmailStatus /></li>
+                        <span className="flex"><img className="sidebar-icon" height="18" src="./apps/email/assets/imgs/inbox.png" />Inbox</span><EmailStatus /></li>
                 </NavLink>
                 <NavLink exact to='/starred'>
-                    <li className="flex"><img className="sidebar-icon" height="18" src="/apps/email/assets/imgs/star.png" /> Starred</li>
+                    <li className="flex"><img className="sidebar-icon" height="18" src="./apps/email/assets/imgs/star.png" /> Starred</li>
                 </NavLink>
                 <NavLink exact to='/sent'>
-                    <li className="flex"><img className="sidebar-icon" height="18" src="/apps/email/assets/imgs/sent.png" /> Sent</li>
+                    <li className="flex"><img className="sidebar-icon" height="18" src="./apps/email/assets/imgs/sent.png" /> Sent</li>
                 </NavLink>
                 <NavLink exact to='/drafts'><li>📝 Drafts</li></NavLink>
             </ul>
