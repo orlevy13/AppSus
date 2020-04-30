d4fe36a3b5c5a671a23dc988da3b4d67349bbe00
michaelmichaeli <michaelmichaeli888@gmail.com>
2020-04-30 03:20:04 +0300

    final

--- apps/notes/cmps/NoteBackground.jsx
+++ apps/notes/cmps/NoteBackground.jsx
@@ -7,7 +7,8 @@ export default class NoteStyle extends React.Component {
 
     render() {
         return (
-            <label> <img className="bgcolor" src="../apps/notes/assets/img/bgcolor.png" />
+            <label>
+                <img className="bgcolor" src="./apps/notes/assets/img/bgcolor.png" />
                 <input hidden type="color" onChange={(ev) => { eventBus.emit('changeBackground', { noteId: this.props.noteId, backgroundColor: ev.target.value }) }} />
             </label>
         )
--- pages/About.jsx
+++ pages/About.jsx
@@ -3,6 +3,14 @@ import eventBus from '../services/eventBusService.js';
 export default function About() {
     eventBus.emit('set-page', { app: 'about' });
     return (
-        <h1>im the about</h1>
+        <React.Fragment>
+            <div className="about">
+
+                <h1>{`<{  Or_Levy_`}</h1>
+                <h1>&&</h1>
+                <h1>{`        _Michael_Michaeli  }>`}</h1>
+                <img src="./assets/img/about.jpg" />
+            </div>
+        </React.Fragment>
     )
 }
