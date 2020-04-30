296d86db172b6e8e44a18c4e16beb3ab3046c567
michaelmichaeli <62397864+michaelmichaeli@users.noreply.github.com>
2020-04-30 03:10:19 +0300

    Update NoteBackground.jsx

--- apps/notes/cmps/NoteBackground.jsx
+++ apps/notes/cmps/NoteBackground.jsx
@@ -4,10 +4,10 @@ export default class NoteStyle extends React.Component {
     state = {
         backgroundColor: ''
     }
-
     render() {
         return (
-            <label> <img className="bgcolor" src="../apps/notes/assets/img/bgcolor.png" />
+            <label> 
+                <img className="bgcolor" src="../apps/notes/assets/img/bgcolor.png" />
                 <input hidden type="color" onChange={(ev) => { eventBus.emit('changeBackground', { noteId: this.props.noteId, backgroundColor: ev.target.value }) }} />
             </label>
         )
